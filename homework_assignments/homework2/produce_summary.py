778895b8c060d499b48fcd03d02c1aa853479d6f
Erin Yaylali <erinyaylali@gmail.com>
2015-01-18 15:44:24 -0800

    Refactored hw2

--- homework_assignments/homework2/produce_summary.py
+++ homework_assignments/homework2/produce_summary.py
@@ -1,25 +1,26 @@
-def print_output(csv):
-    my_file = open(csv)
+files_by_day = ["um-deliveries-20140519.csv","um-deliveries-20140520.csv","um-deliveries-20140521.csv"]
+
+
+def report_content(day_number, path):
+    print "Day %d" % day_number
+    my_file = open(path, "r")
     for line in my_file:
         line = line.rstrip()
         words = line.split(',')
-    
+
         melon = words[0]
         count = words[1]
         amount = words[2]
-    
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+        
+        print ("Delivered %s %ss for total of $%s" % (
+            count, melon, amount)).upper()
     my_file.close()
 
-# Day 1
-print "Day 1"
-print_output("um-deliveries-20140519.csv")
+    print
 
-# Day 2
-print "Day 2"
-print_output("um-deliveries-20140520.csv")
+def main():
+	for day in range(len(files_by_day)):
+		report_content((day+1), files_by_day[day])
 
-# Day 3
-print "Day 3"
-print_output("um-deliveries-20140521.csv")
 
+main()
