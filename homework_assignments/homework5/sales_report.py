07f78d3e6f01690acfe3dc59b67ce8e4dd3e1af8
Erin Yaylali <erinyaylali@gmail.com>
2015-01-18 17:51:10 -0800

    Completed hw5

--- homework_assignments/homework5/sales_report.py
+++ homework_assignments/homework5/sales_report.py
@@ -4,23 +4,79 @@ sales_report.py - Generates sales report showing the total number
 
 """
 
-salespeople = []
-melons_sold = []
+def melon_results(path):
+    the_file = open(path)
+    melon_results = {}
 
-f = open("sales_report.csv")
-for line in f:
-    line = line.rstrip()
-    entries = line.split(",")
-    salesperson = entries[0]
-    melons = int(entries[2])
+    for line in the_file:
+        line = line.rstrip()
+        sales_info = line.split(",")
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
+        sales_rep = sales_info[0]
+        revenue = float(sales_info[1])
+        melons_sold = int(sales_info[2])
+
+        if sales_rep in melon_results:
+            melon_results[sales_rep] += melons_sold
+        else:
+            melon_results[sales_rep] = melons_sold
+    print list(melon_results.items())
+
+def revenue_results(path):
+    the_file = open(path)
+    revenue_results = {}
+
+    for line in the_file:
+        line = line.rstrip()
+        sales_info = line.split(",")
+
+        sales_rep = sales_info[0]
+        revenue = float(sales_info[1])
+        melons_sold = int(sales_info[2])
+    
+
+        if sales_rep in revenue_results:
+            revenue_results[sales_rep] += revenue
+        else:
+            revenue_results[sales_rep] = revenue
+    print list(revenue_results.items())
+
+def main(sort_by,path):
+    if sort_by == "melons":
+        melon_results(path)
+    elif sort_by == "revenue":
+        revenue_results(path)
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+        print "Please enter valid sorting type."
+
+main("melons", "sales_report.csv")
 
 
-for i in range(len(salespeople)):
-    print "%s sold %d melons" % (salespeople[i], melons_sold[i])
+
+
+
+
+
+
+# salespeople = []
+# melons_sold = []
+
+
+
+# f = open("sales_report.csv")
+# for line in f:
+#     line = line.rstrip()
+#     entries = line.split(",")
+#     salesperson = entries[0]
+#     melons = int(entries[2])
+
+#     if salesperson in salespeople:
+#         position = salespeople.index(salesperson)
+#         melons_sold[position] += melons
+#     else:
+#         salespeople.append(salesperson)
+#         melons_sold.append(melons)
+
+
+# for i in range(len(salespeople)):
+#     print "%s sold %d melons" % (salespeople[i], melons_sold[i])
