388a7ab95835216701647bf0b2736719acab2cbc
Erin Yaylali <erinyaylali@gmail.com>
2015-01-18 18:35:46 -0800

    Refactored code

--- guessing_game/guess.py
+++ guessing_game/guess.py
@@ -12,40 +12,37 @@ def check_int(userInput):
 def check_range(userInput): 
     if userInput > 101 or userInput < 0:
         print "Please enter an integer between 1 and 100!!"
-        return False
     else:
         return True
 
+def main():
+    name = str(raw_input("Hello, what is your name? "))
+    print name + ", I'm thinking of a number between 1 and 100. Try to guess my number."
+    the_number = random.randint(1, 100)
+    count = 1
+    guess = 0
 
+    print(the_number)
 
-name = str(raw_input("Hello, what is your name? "))
-print name + ", I'm thinking of a number between 1 and 100. Try to guess my number."
-
-the_number = random.randrange(1, 101)
-count = 0
-guess = 0
-
-print(the_number)
-
-while guess != the_number:
-
-    guess = raw_input("Your guess? ")
+    while guess != the_number:
+        guess = raw_input("Your guess? ")
     
-    if check_int(guess) == True:
-        guess = int(guess)
-    else:
-        continue
-
-    if check_range(guess) == True:
-    
-        if guess < the_number:
-            print "Your guess is too low, try again."
-            count += 1
-            continue
-        elif guess > the_number:
-            print "Your guess is too high, try again."
-            count += 1
-            continue
+        if check_int(guess) == True:
+            guess = int(guess)
         else:
-            print "Well done, " + name + "! You found my number in " + str(count) + " tries!"
+            continue
+
+        if check_range(guess) == True:
+            if guess < the_number:
+                print "Your guess is too low, try again."
+                count += 1
+            elif guess > the_number:
+                print "Your guess is too high, try again."
+                count += 1
+            else:
+                print "Well done, " + name + "! You found my number in " + str(count) + " tries!"
+        else:
+            continue
+
+main()
 
