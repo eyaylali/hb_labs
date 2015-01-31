52f995869807c2742fb4a7320a1a2b7ea7f2ff7b
Erin Yaylali <erinyaylali@gmail.com>
2015-01-30 17:34:21 -0800

    adding while loop to loop through list, implementing setdefault

--- reddit_challenge/in_words.py
+++ reddit_challenge/in_words.py
@@ -1,7 +1,5 @@
 from sys import argv
-
-    # dictionary = set(word.strip() for word in words if len(word.strip()) > 2)
-
+from collections import defaultdict
 
 def unpack(path):
 	working_file = open(path)
@@ -9,8 +7,8 @@ def unpack(path):
 	working_list = []
 
 	for line in working_file:
-		word = line.split()
-		working_list += word
+		word = line.strip()
+		working_list.append(word)
 	return working_list
 
 def iter_list(working_list):
@@ -19,12 +17,13 @@ def iter_list(working_list):
 	working_dict = {}
 
 	counter = 0
-	for i in range(len(working_list[counter])):
-		if len(working_list[counter]) > 2: #not sure about placement
-			for j in range(i+2, (len(working_list[counter])+1)):
-				if working_list[counter][i:j] in working_set:
-					dict2 = working_dict.setdefault(working_list[counter], 0) + 1
-			counter += 1
+	while counter < len(working_list):
+		for i in range(len(working_list[counter])):
+				for j in range(i+2, (len(working_list[counter])+1)):
+					# print working_list[counter][i:j]
+					if working_list[counter][i:j] in working_set:
+						working_dict.setdefault(working_list[counter], 0) + 1 #not working
+		counter += 1
 
 	return working_dict.items()
 
@@ -32,40 +31,12 @@ def return_max(final_list):
 	output = max(final_list, key=lambda x: x[1])
 	return output
 
-	# working_dict = {}
-	# words_with_most_words = []
-	# word_quantity = 0
-
-
-	# counter = 0
-	# checked_word = working_list[counter]
-
-	# for word1 in (working_list[:counter] and working_list[counter:]):
-	# 	if word1 in checked_word and word1 != checked_word and word1 >=2:
-	# 		if checked_word in working_dict:
-	# 			working_dict[checked_word] += 1
-	# 			counter += 1
-	# 		else:
-	# 			working_dict[checked_word] = 1
-
-	# for quantity in working_dict.values():
-	# 	if quantity > word_quantity:
-	# 		word_quantity = quantity
-
-	# for word, quantity in working_dict.items():
-	# 	if quantity == word_quantity:
-	# 		words_with_most_words += [word]
-
-	# return words_with_most_words, word_quantity
-
 def main(input_file):
 	working_list = unpack(input_file)
 	words_with_count = iter_list(working_list)
 	list_most_words = return_max(words_with_count)
 	print list_most_words
 
-
 if __name__ == "__main__":
     input_file = argv[1]
     main(input_file)
-
