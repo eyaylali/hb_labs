ad427f79db64df8c25c7b1cb382047564824e81e
Erin Yaylali <erinyaylali@gmail.com>
2015-02-01 00:11:14 -0800

    1. more cleaning up - consolidating functions into main function and removed unecessary functions (unpack, return_max), used comprehension to make count_of_subwords more succinct and clear

--- reddit_challenge/README.md
+++ reddit_challenge/README.md
@@ -1,3 +1,3 @@
 My solution to reddit/dailyprogrammer <a href=http://www.reddit.com/r/dailyprogrammer/comments/2nihz6/20141126_challenge_190_intermediate_words_inside>
-challenge #190</a>(intermediate).
+challenge #190</a> (intermediate).
 
--- reddit_challenge/in_words.py
+++ reddit_challenge/in_words.py
@@ -1,32 +1,14 @@
 from sys import argv
 
-def unpack(path):
-    return {w.strip() for w in open(path)}
-
-def iter_list(working_set):
-
-    working_dict = {}
-
-    for counter, word in enumerate(working_set):
-        for i in range(len(word) - 1):
-            for j in range(i + 2, (len(word) + 1)):
-                if word[i:j] in working_set:
-                    working_dict[word] = working_dict.get(word, 0) + 1
-
-    return working_dict
-
-def return_max(final_list):
-    output = max(final_list, key=lambda x: final_list[x])
-    return output
-
 def main(input_file):
-    working_list = unpack(input_file)
-    words_with_count = iter_list(working_list)
-    list_most_words = return_max(words_with_count)
-    print list_most_words
+
+	def count_of_subwords(word):
+		return sum(word[i:j] in words for i in range(len(word)-1) for j in range(i+ 2, len(word) +1))
+
+	words = {word.strip() for word in open(input_file)}
+	print max(words, key=count_of_subwords)
 
 if __name__ == "__main__":
     input_file = argv[1]
     main(input_file)
-
 
